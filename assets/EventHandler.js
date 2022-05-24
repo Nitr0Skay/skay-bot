7d5d1b177f9560b0ba5a6b7d5137d31eb8199b02
Marcin <cin.miko94@gmail.com>
2022-05-24 23:23:04 +0200

    Fix and Fill the Event Handler file

--- assets/EventHandler.js
+++ assets/EventHandler.js
@@ -1,3 +1,23 @@
-const EventHandler = function(event, callback, options) {
-  
-};
+const EventHandler = (function(element, event, callback, options) {
+  if(addEventListener) {
+    return function(element, event, callback, options) {
+      element.addEventListener(event, function(e) {
+        callback.call(this, e || window.event);
+      }, options);
+    };
+  } else if(attachEvent) {
+    return function(element, event, callback) {
+      event = 'on' + event;
+      element.attachEvent(event, function(e) {
+        callback.call(this, e || window.event);
+      });
+    };
+  } else {
+    return function(element, event, callback) {
+      event = 'on' + event;
+      element[event] = function(e) {
+        callback.call(this, e || window.event);
+      };
+    };
+  }
+})();
