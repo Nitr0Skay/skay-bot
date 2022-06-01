19cf375b9ad307d73f245e1d2642302a8b7f0348
Marcin <cin.miko94@gmail.com>
2022-06-01 04:56:00 +0200

    Bugfix the Event Handler

--- assets/EventHandler.js
+++ assets/EventHandler.js
@@ -1,23 +1,17 @@
-const EventHandler = (function(element, event, callback, options) {
+const EventHandler = function(element, event, callback, options) {
   if(addEventListener) {
-    return function(element, event, callback, options) {
-      element.addEventListener(event, function(e) {
-        callback.call(this, e || window.event);
-      }, options);
-    };
+    element.addEventListener(event, function(e) {
+      callback.call(this, e || window.event);
+    }, options);
   } else if(attachEvent) {
-    return function(element, event, callback) {
-      event = 'on' + event;
-      element.attachEvent(event, function(e) {
-        callback.call(this, e || window.event);
-      });
-    };
+    event = 'on' + event;
+    element.attachEvent(event, function(e) {
+      callback.call(this, e || window.event);
+    });
   } else {
-    return function(element, event, callback) {
-      event = 'on' + event;
-      element[event] = function(e) {
-        callback.call(this, e || window.event);
-      };
+    event = 'on' + event;
+    element[event] = function(e) {
+      callback.call(this, e || window.event);
     };
   }
-})();
+};
