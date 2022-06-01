49a542425d2868204d139f26b689f86ab5cd2a1b
Marcin <cin.miko94@gmail.com>
2022-06-01 05:00:47 +0200

    Skay Bot asking the name

--- skay-bot.js
+++ skay-bot.js
@@ -1,5 +1,19 @@
 (function() {
+  const Gall = 'Anonim';
   const Skay = new Bot();
+  const botBox = Skay.createChatBox(document.body);
 
-  Skay.greeting(document.body);
+  Skay.greeting(botBox);
+  EventHandler(Skay.answerBox, 'click', function(e) {
+
+  if(e.target.innerHTML === 'Tak') {
+      Skay.askName(e.target.parentElement, Skay.answerBox);
+    } else if(e.target.innerHTML === 'Nie') {
+      name = Gall;
+    }
+  }, {
+    useCapture: false,
+    once: true
+  });
+
 })();
