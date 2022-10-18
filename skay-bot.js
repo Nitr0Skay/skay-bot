136954ab902b137c96a7704bc331e5ac4fec7f11
Marcin <cin.miko94@gmail.com>
2022-10-19 01:52:30 +0200

    Change Simple Strings to Template Literals. Skay Bot will now wait almost every single dialogue line

--- skay-bot.js
+++ skay-bot.js
@@ -1,20 +1,20 @@
 (function() {
-  const Skay = new Bot('Skay');
-  const Gall = new String('Anonim');
+  const Skay = new Bot(`Skay`);
+  const Gall = new String(`Anonim`);
   const botBox = Skay.createChatBox(document.body);
 
   Skay.greeting(botBox);
   const promise = new Promise((resolve, reject) => {
-    EventHandler(Skay.answerBox, 'click', function(e) {
+    EventHandler(Skay.answerBox, `click`, function(e) {
 
-      if(e.target.innerHTML === 'Tak') {
+      if(e.target.innerHTML === `Tak`) {
           Skay.askName(e.target.parentElement, Skay.answerBox);
-          const nameSubmitButton = document.getElementById('submitName');
-          EventHandler(nameSubmitButton, 'click', function(e) {
+          const nameSubmitButton = document.getElementById(`submitName`);
+          EventHandler(nameSubmitButton, `click`, function(e) {
             e.preventDefault();
-            resolve(document.getElementById('imie').value);
+            resolve(document.getElementById(`imie`).value);
           }, false);
-        } else if(e.target.innerHTML === 'Nie') {
+        } else if(e.target.innerHTML === `Nie`) {
           resolve(Gall);
         }
       }, {
@@ -23,13 +23,13 @@
       });
   });
 
-  promise.then((name) => {
+  promise.then(async (name) => {
     const user = new User(name);
 
-    Skay.answerBox.remove();
-    Skay.saySomething(new String('Bardzo mi miło Ciebie poznać'), botBox);
-    Skay.saySomething(new String('Jak mogę Tobie pomóc ? :D'), botBox);
-    Skay.saySomething(new String('Poniżej podam listę pomocnych linków:'), botBox);
-    Skay.createHelpList(botBox);
+    await Skay.answerBox.remove();
+    await Skay.saySomething(new String(`Bardzo mi miło Ciebie poznać, panie`), botBox);
+    await Skay.saySomething(new String(`Jak mogę Tobie pomóc ? :D`), botBox);
+    await Skay.saySomething(new String(`Poniżej podam listę pomocnych linków:`), botBox);
+    await Skay.createHelpList(botBox);
   });
 })();
