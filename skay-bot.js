8d3384dcd557bdef840ef8554a26d72d31919010
Marcin <cin.miko94@gmail.com>
2022-11-14 04:32:56 +0100

    Fill up the second answerBox for Skay instance. We are ready to create Phase Three now

--- skay-bot.js
+++ skay-bot.js
@@ -1,4 +1,5 @@
 (function() {
+  //  Phase 1
   const Skay = new Bot(`Skay`);
   const Gall = new String(`Anonim`);
   const botBox = Skay.createChatBox(document.body);
@@ -23,14 +24,21 @@
       });
   });
 
-  promise.then(async (name) => {
+  promise.then(async (name) => {  //  Phase 2
     const user = new User(name);
     const userContener = document.createElement(`u`);
           userContener.classList.add('userName');
           userContener.innerHTML = user.name;
 
-    await Skay.answerBox.remove();
+    Skay.answerBox.remove();
     await Skay.saySomething(new String(`Bardzo mi miło Ciebie poznać, panie `), botBox, userContener);
+
+    if(name !== Gall) {
+      await Skay.saySomething(new String(`Czy zapiszesz się na nasz newsletter ? Wystarczy tylko podać adres e-mail`), botBox);
+      Skay.answerBox = Skay.createAnswerBox([`Pewnie, przyda się`, `Raczej podziękuję`]);
+      console.log(Skay.answerBox);
+    }
+
     await Skay.saySomething(new String(`Jak mogę Panu pomóc ? :D`), botBox);
     await Skay.saySomething(new String(`Poniżej podam listę pomocnych linków:`), botBox);
     await Skay.createHelpList(botBox);
